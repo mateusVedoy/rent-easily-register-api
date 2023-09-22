f756042d8ea7bf7051472c989c2ce27adc31a665
mateusVedoy <mateusvedoy@outlook.com>
2023-09-22 10:35:20 -0300

    feat: add generic shared repository

---
+++ src/main/java/rent/easily/shared/infrastructure/Repository.java
@@ -0,0 +1,37 @@
+package rent.easily.shared.infrastructure;
+
+import java.util.List;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import rent.easily.shared.domain.port.IRepository;
+
+public abstract class Repository<T1, T2> implements PanacheRepository<T2>, IRepository<T1, T2> {
+
+    @Override
+    public void save(T1 entity) {
+        T2 model = convertToModel(entity);
+        persist(model); //depois add uma validação que salvou realmente com isPersistent
+    }
+
+    @Override
+    public T1 findPerId(Long id) {
+        T2 model = findById(id);
+        return convertToDomainList(List.of(model)).get(0); //refact depois
+    }
+
+    @Override
+    public List<T1> getAll() {
+        List<T2> models = findAll().list();
+        return convertToDomainList(models);
+    }
+
+    @Override
+    public void editById(Long id, T1 entity) { //estudar forma melhor de editar
+        deleteById(id);
+        T2 model = convertToModel(entity);
+        persist(model);
+    }
+
+    protected abstract T2 convertToModel(T1 entity);
+    protected abstract List<T1> convertToDomainList(List<T2> models);
+}
