3a9ea36caf23320c5bce6dae61fd0718d0bc3572
RodrigoPresa <rodrigo_presa@outlook.com>
2023-09-24 17:25:33 +0000

    criando a entidade Property

---
+++ src/main/java/rent/easily/property/domain/RegisterType.java
@@ -0,0 +1,17 @@
+package rent.easily.property.domain;
+
+public enum RegisterType {
+    LESSEE(1L),
+    LESSOR(2L);
+
+    private final Long value;
+
+
+    RegisterType(Long value){
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return this.value;
+    }
+}
