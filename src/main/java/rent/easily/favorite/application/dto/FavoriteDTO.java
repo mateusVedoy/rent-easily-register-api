559d91bdc8366b08bb797f4b41c99aaaeadf519a
mateusVedoy <mateusvedoy@outlook.com>
2023-09-25 17:23:00 -0300

    feat: favorite dto

--- src/main/java/rent/easily/favorite/application/dto/FavoriteDTO.java
+++ src/main/java/rent/easily/favorite/application/dto/FavoriteDTO.java
@@ -9,14 +9,14 @@ public class FavoriteDTO {
     private Long id;
     private Long advertisementId;
     private Long userId;
-    private DateTime favoritedAt;
+    private DateTimeDTO favoritedAt;
 
-    public FavoriteDTO(Long ad, Long userId, DateTime dateTime) {
+    public FavoriteDTO(Long ad, Long userId, DateTimeDTO dateTime) {
         this.advertisementId = ad;
         this.userId = userId;
         this.favoritedAt = dateTime;
     }
-    public FavoriteDTO(Long id, Long ad, Long userId, DateTime dateTime) {
+    public FavoriteDTO(Long id, Long ad, Long userId, DateTimeDTO dateTime) {
         this.id = id;
         this.advertisementId = ad;
         this.userId = userId;
