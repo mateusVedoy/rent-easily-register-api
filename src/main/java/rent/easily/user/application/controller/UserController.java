5af77aac3ca4c17681409d66013fa4c11393d77c
mateusVedoy <mateusvedoy@outlook.com>
2023-10-02 19:43:45 -0300

    feat. update entity flow

--- src/main/java/rent/easily/user/application/controller/UserController.java
+++ src/main/java/rent/easily/user/application/controller/UserController.java
@@ -3,17 +3,21 @@ package rent.easily.user.application.controller;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import rent.easily.shared.application.response.APIResponse;
+import rent.easily.shared.application.response.ResponseError;
+import rent.easily.shared.application.response.ResponseSuccess;
 import rent.easily.shared.application.useCase.GetAllEntities;
 import rent.easily.shared.application.useCase.GetEntityById;
 import rent.easily.shared.domain.port.IConvert;
 import rent.easily.user.application.dto.UserDTO;
 import rent.easily.user.application.useCase.CreateUser;
+import rent.easily.user.application.useCase.UpdateUser;
 import rent.easily.user.domain.User;
 import rent.easily.user.infrastructure.database.UserModel;
 import rent.easily.user.infrastructure.database.UserRepository;
@@ -34,6 +38,8 @@ public class UserController {
     GetAllEntities<UserDTO, User, UserModel> getAllEntities;
     @Inject
     GetEntityById<UserDTO, User, UserModel> getEntityById;
+    @Inject
+    UpdateUser updateUser;
     
     @POST
     @Path("/create")
@@ -57,5 +63,11 @@ public class UserController {
         return getEntityById.execute(id, repository, convertToDomain, convertToDTO);
     }
 
-    //falta o editar por Id
+    @PATCH
+    @Path("/update/id/{identifier}")
+    @Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+    public APIResponse update(@PathParam("identifier") String identifier, UserDTO user) {
+        Long id = Long.parseLong(identifier);
+        return updateUser.execute(user, id);
+    }
 }
---
+++ src/main/java/rent/easily/user/application/useCase/UpdateUser.java
@@ -0,0 +1,26 @@
+package rent.easily.user.application.useCase;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import rent.easily.shared.application.response.APIResponse;
+import rent.easily.shared.application.useCase.UpdateEntity;
+import rent.easily.user.application.dto.UserDTO;
+import rent.easily.user.application.service.UserNormalize;
+import rent.easily.user.domain.User;
+import rent.easily.user.infrastructure.database.UserModel;
+import rent.easily.user.infrastructure.database.UserRepository;
+
+@ApplicationScoped
+public class UpdateUser {
+
+    @Inject
+    UserRepository repository;
+    @Inject
+    UpdateEntity<UserDTO, User, UserModel> updateEntity;
+    @Inject
+    UserNormalize normalizer;
+
+    public APIResponse execute(UserDTO dto, Long id) {
+       return updateEntity.execute(id, dto, repository, normalizer);
+    }   
+}
