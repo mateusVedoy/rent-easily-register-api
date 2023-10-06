d1c12d4fe7254778c0847342303fa6120827f480
mateusVedoy <mateusvedoy@outlook.com>
2023-10-06 20:47:39 +0000

    feat: create proposal spec validate user existence and proposal existence

--- src/main/java/rent/easily/proposal/domain/specification/CreateProposalSpec.java
+++ src/main/java/rent/easily/proposal/domain/specification/CreateProposalSpec.java
@@ -1,10 +1,12 @@
 package rent.easily.proposal.domain.specification;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import rent.easily.proposal.domain.entity.Proposal;
+import rent.easily.proposal.infrastructure.database.ProposalRepository;
 import rent.easily.shared.domain.exception.BusinessException;
 import rent.easily.shared.domain.exception.ValidationError;
 import rent.easily.shared.domain.port.ICriteria;
@@ -15,17 +17,36 @@ public class CreateProposalSpec implements ICriteria<Proposal> {
 
     @Inject
     UserRepository userRepository;
+    @Inject
+    ProposalRepository proposalRepository;
+
+    private List<BusinessException> errors;
 
     @Override
     public void validate(Proposal entry) throws ValidationError {
+
+        errors = new ArrayList<>();
         // TODO: valida exist de anuncio
 
         if (!isThereUserForGivenId(entry.getUserId()))
-            throw new ValidationError(
-                    List.of(new BusinessException("There's no User for given userId", "domain.User.id")));
+            errors.add(new BusinessException("There's no User for given userId", "domain.User.id"));
+
+        if(isThereProposalForGivenUserAndAdd(entry.getUserId(), entry.getAdvertisementId()))
+            errors.add(new BusinessException("There's already Proposal for given user and add", "domain.Proposal"));
+
+        if(hasErrors())
+            throw new ValidationError(errors);
+    }
+
+    private boolean hasErrors() {
+        return errors.size() > 0;
     }
 
     private boolean isThereUserForGivenId(Long id) {
-        return userRepository.existsUserById(id);
+       return userRepository.existsUserById(id);
+    }
+
+    private boolean isThereProposalForGivenUserAndAdd(Long userId, Long add) {
+       return proposalRepository.existsProposalsForUserAndAdd(userId, add);
     }
 }
